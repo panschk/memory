0476f05dc295595c30450229335d6476f1b001fe
panschk <panschk@googlemail.com>
2016-11-16 21:01:44 +0100

    multi language support, attribution, sound

--- data.js
+++ data.js
@@ -1,1 +1,23 @@
-var data = ["accesory-1.png","accesory-4.png","airplane.png","apple.png","armchair.png","backpack.png","basketball.png","boards.png","boat.png","books.png","box.png","bus.png","calculator-1.png","car.png","cat.png","chemistry.png","clock.png","computer.png","cow.png","cup.png","cycling.png","dog.png","earth-globe-1.png","eyeglasses.png","footwear.png","frog.png","koala.png","ladybug.png","lion.png","magician.png","microscope.png","pen.png","pencil.png","pig.png","raccoon.png","science.png","scissors.png","search.png","smartphone.png","spanish-guitar.png","target.png","telescope.png","train-2.png","ufo.png","wasp.png"]
+var icons = ["accesory-1.png","accesory-4.png","airplane.png","apple.png","armchair.png","backpack.png","basketball.png","boards.png","boat.png","books.png","box.png","bus.png","calculator-1.png","car.png","cat.png","chemistry.png","clock.png","computer.png","cow.png","cup.png","cycling.png","dog.png","earth-globe-1.png","eyeglasses.png","footwear.png","frog.png","koala.png","ladybug.png","lion.png","magician.png","microscope.png","pen.png","pencil.png","pig.png","raccoon.png","science.png","scissors.png","search.png","smartphone.png","spanish-guitar.png","target.png","telescope.png","train-2.png","ufo.png","wasp.png"]
+var languages = [
+{code:"en", name:"English"},
+{code:"de", name:"Deutsch"},
+{code:"fr", name:"Français"},
+
+]
+var translate = function(code, lang) {
+
+	var texts = {
+		highscore 	: {de: "Bestenliste", en: "Hall of Fame", fr: "Classement"},
+		play		: {de: "Spielen", en: "Play!", fr: "Jouer"},
+		pairs		: {de: "Paare", en: "pairs", fr: "paires"},
+		cards_turned: {de: "Du hast alle Karte in {0} Zügen aufgedeckt!\nWie heißt Du?", en: "You matched all cards in {0} turns.\nWhat's your name?", fr: "Tu as trouvé tout les pairs en {0} tours.\nComment tu t'appelles?"},
+		back		: {de: "Zurück", en: "Back", fr: "Retour"},
+
+	}
+	var valForLang = texts[code][lang.code];
+	if (valForLang) {
+		return valForLang;
+	}
+	return texts[code]['en']; 
+}
--- elefantbrain.js
+++ elefantbrain.js
@@ -2,7 +2,7 @@ HIGHSCORE_SIZE=3;
 var app = angular.module('memory', []);
 app.controller('Main', ['$scope', function Main($scope) {
 	this.isactive = 'menu';
-	this.count=4;
+	this.count=g.lastCount;
 	this.startMemory = function() {
 		this.isactive = 'memory';
 		$scope.game = new Memory(this);
@@ -14,27 +14,30 @@ app.controller('Main', ['$scope', function Main($scope) {
 	this.getCardsize = function() {
 		return "height:"+(25/Math.log(this.count))+"vw;width: "+(25/Math.log(this.count))+"vw;";
 	}
-	
-	$scope.data=data
-	$scope.globalScope=globalScope;
+	$scope.g=g;
+	$scope.translate = translate;
+	$scope.languages = languages;
+	$scope.text = text;
 }]);
 
 var Memory = function(m) {
 	this.m = m;
 	this.init = function(count) {
-		globalScope.turns = 0;
+		g.turns = 0;
+		g.lastCount = count;
+		save();
 		this.turnedCards = [];
 		this.count = count;
 		this.cards = new Array(this.count * 2);
-		var indexSel = getRandomIndex(data.length, this.count);
+		var indexSel = getRandomIndex(icons.length, this.count);
 		for (var i=0; i < indexSel.length; i++) {
-			this.cards[i*2] = {index:indexSel[i], value:data[indexSel[i]],class:'hidden'};
-			this.cards[i*2+1] = {index:indexSel[i], value:data[indexSel[i]],class:'hidden'};
+			this.cards[i*2] = {index:indexSel[i], value:icons[indexSel[i]],class:'hidden'};
+			this.cards[i*2+1] = {index:indexSel[i], value:icons[indexSel[i]],class:'hidden'};
 		}
 		shuffle(this.cards);
 	}
 	this.turn = function(index) {
-		globalScope.turns++;
+		g.turns++;
 		if (this.turnedCards.length > 1) {
 			this.cards.forEach(function(card) {
 				if (card.class=='shown') {
@@ -52,8 +55,9 @@ var Memory = function(m) {
 			if (c1.index===c2.index) {
 				c1.class='done';
 				c2.class='done';
+				noise();
+				this.checkAllTurned();
 			}
-			this.checkAllTurned();
 		}
 	}
 	this.checkAllTurned = function(){
@@ -65,61 +69,74 @@ var Memory = function(m) {
 		}
 		// all cards were found!
 
-		if (globalScope.isEligble(this.count, globalScope.turns)) {
-			var username = prompt("Du hast alle Karte in " + globalScope.turns 
-				+ " Zügen aufgedeckt!\nWie heißt Du?", globalScope.lastName);
+		if (isEligble(this.count, g.turns)) {
+			var username = prompt(text('cards_turned').format(g.turns), g.lastName);
 			if (username)  {
-				globalScope.lastName=username;
-				var entry = new HighScoreEntry(username, globalScope.turns);
-				globalScope.addToList(this.count, entry);
+				g.lastName=username;
+				var entry = new HighScoreEntry(username, g.turns);
+				addToList(this.count, entry);
 			}
 		}
 		m.mainMenu();
 	}
 }
 
-var GlobalScope = function() {
-	this.lastName = Lockr.get("lastname"); ;
-	this.turns = 0;
-	this.highscoreList = Lockr.get("highscoreList");
-	if (!this.highscoreList) {
-		this.highscoreList = {};
+var load = function() {
+	var gs = Lockr.get("elefantbrain-globalscope");
+	if (!gs) {
+		gs = GlobalScope;
 	}
+	return gs;
+}
 
-	this.isEligble = function (size, turns) {
-		if (!this.highscoreList[size]) {
-			return true;
-		}
-		if (this.highscoreList[size].length < HIGHSCORE_SIZE) {
-			return true;
-		}
-		if (this.highscoreList[size][HIGHSCORE_SIZE-1].turns > turns) {
-			return true;
-		}
-		return false;
-	}
-	this.addToList = function(size, entry) {
-		if (!this.highscoreList[size]) {
-			this.highscoreList[size] = [entry];
-		} else {
-			
-			for (var i = 0; i < this.highscoreList.size; i++) {
-				if (this.highscoreList[size][i].turns < entry.turns) {
-					this.highscoreList[size].splice(i, 0, entry);
-					var wasAdded = true;
-				}
-			}
-			if (!wasAdded) {
-				this.highscoreList[size].push(entry);
-			}
-			if (this.highscoreList[size].length > HIGHSCORE_SIZE) {
-				this.highscoreList[size].splice(0, HIGHSCORE_SIZE -1);
-			}
-		}
+var save = function() {
+	Lockr.set("elefantbrain-globalscope", g);
+}
 
-		Lockr.set("lastname", entry.name);
-		Lockr.set("highscoreList",this.highscoreList);
+var text = function(t) {
+	return translate(t, g.language);
+}
+var GlobalScope = {
+	lastName : "",
+	turns : 0,
+	highscoreList : {},
+	lastCount : 4,
+	language : languages[0]
+}
+
+var isEligble = function (size, turns) {
+	if (!g.highscoreList[size]) {
+		return true;
 	}
+	if (g.highscoreList[size].length < HIGHSCORE_SIZE) {
+		return true;
+	}
+	if (g.highscoreList[size][HIGHSCORE_SIZE-1].turns > turns) {
+		return true;
+	}
+	return false;
+}
+var addToList = function(size, entry) {
+	if (!g.highscoreList[size]) {
+		g.highscoreList[size] = [entry];
+	} else {
+		
+		for (var i = 0; i < g.highscoreList[size].length; i++) {
+			if (g.highscoreList[size][i].turns > entry.turns) {
+				g.highscoreList[size].splice(i, 0, entry);
+				var wasAdded = true;
+				break;
+			}
+		}
+		if (!wasAdded) {
+			g.highscoreList[size].push(entry);
+		}
+		if (g.highscoreList[size].length > HIGHSCORE_SIZE) {
+			g.highscoreList[size].splice(0, HIGHSCORE_SIZE -1);
+		}
+	}
+	save();
+	
 }
 
 var HighScoreEntry = function(name, turns) {
@@ -128,7 +145,7 @@ var HighScoreEntry = function(name, turns) {
 }
 
 
-globalScope = new GlobalScope();
+g = load();
 
 function getRandomIndex(len, n) {
     var result = new Array(n),
@@ -159,4 +176,35 @@ function shuffle(a) {
         a[i - 1] = a[j];
         a[j] = x;
     }
+}
+
+function noise() {
+    var f1 = getRandomInt(200,800)
+	var f2 = getRandomInt(200,800)
+	
+	var sine1 = T("sin", {freq:f1, mul:0.5});
+	var sine2 = T("sin", {freq:f2, mul:0.5});
+    T("perc", {r:300}, sine1, sine2).on("ended", function() {
+	this.pause();
+	}).bang().play();
+}
+function getRandomInt(min, max) {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min)) + min;
+}
+
+// Implement useful String format method
+// http://stackoverflow.com/questions/610406/javascript-equivalent-to-printf-string-format
+// First, checks if it isn't implemented yet.
+if (!String.prototype.format) {
+  String.prototype.format = function() {
+    var args = arguments;
+    return this.replace(/{(\d+)}/g, function(match, number) { 
+      return typeof args[number] != 'undefined'
+        ? args[number]
+        : match
+      ;
+    });
+  };
 }
